c862e716f0f9e4efca732f3aaaf80a22bee16274
akshar13122 <164928413+akshar13122@users.noreply.github.com>
2024-06-16 23:12:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function showSection(sectionId) {
+    document.querySelectorAll('main section').forEach(section => {
+        section.classList.add('hidden');
+    });
+    document.getElementById(sectionId).classList.remove('hidden');
+}
